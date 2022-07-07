e98df85598941350043695beac129a12c087cc42
KeZhuocheng <106873405+KeZhuocheng@users.noreply.github.com>
2022-07-07 22:48:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -7,14 +7,7 @@ int main()
     int b=4;
     int c;
     c=a|b;
-
     printf("%d",c);
-
-  /*  int a=45;
-    int b=0;
-    int c=10;
-    c=(a+b,b+c,a+c);
-    printf("%d",c);*/
-
+    
     return 0;
 }
